44b44a09a0753fe42855fb17badc71ea6db26437
Chip Carnes <chip.carnes27@gmail.com>
2016-10-05 10:32:46 -0700

    Remove stuff

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,19 +1,10 @@
 class Account < ApplicationRecord
-  # attr_accessible :avatar
 
   validates_presence_of :first_name, :last_name
   # has_and_belongs_to_many :events
   belongs_to :user
 
   #paperclip
-  has_attached_file :avatar,
-                    # :storage => :s3,
-                    # :s3_permissions => 'authenticated-read',
-                    # :s3_protocol => "http",
-                    styles: {medium: "500x", thumb: "100x100>"},
-                    # default_url: "/images/missing.png",
-                    :path => "erefuge/:attachment/:style/:id.:extension",
-                    # :bucket => "erefuge"
-                    # :s3_credentials => S3_CREDENTIALS
+  has_attached_file :avatar, styles: {medium: "500x", thumb: "100x100>"}, default_url: "/images/missing.png", :path =>"erefuge/:attachment/:style/:id.:extension"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
