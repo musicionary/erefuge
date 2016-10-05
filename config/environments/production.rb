775de21fbcbf33dc85d45cadb9dd583e7d14b316
Chip Carnes <chip.carnes27@gmail.com>
2016-10-04 21:29:40 -0700

    add aws config to production for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,14 @@ Rails.application.configure do
                                                  'http://erefuge.herokuapp.com']
 
   config.action_cable.url = "wss://erefuge.herokuapp.com/cable"
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
 end
