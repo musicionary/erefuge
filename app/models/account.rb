295a610f3271f31ed92b9abefe6273fc5491d355
Chip Carnes <chip.carnes27@gmail.com>
2016-10-05 10:00:16 -0700

    Add more info to account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,6 +4,12 @@ class Account < ApplicationRecord
   belongs_to :user
 
   #paperclip
-  has_attached_file :avatar, styles: {medium: "500x", thumb: "100x100>"}, default_url: "/images/missing.png", :path => "erefuge/:attachment/:style/:id.:extension"
+  has_attached_file :avatar,
+                    :storage => :s3,
+                    :s3_permissions => 'authenticated-read',
+                    :s3_protocol => "http",
+                    styles: {medium: "500x", thumb: "100x100>"},
+                    default_url: "/images/missing.png",
+                    :path => "erefuge/:attachment/:style/:id.:extension"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
